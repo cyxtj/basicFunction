af41850c91f4117c44af4f976b3ad7d619ee372e
jinjiandong <jjdblast@gmail.com>
2013-04-19 21:35:56 +0800

    let's merge

--- veterans/main.py
+++ veterans/main.py
@@ -7,9 +7,9 @@ veteransApp.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:levis822@localhost
 
 @veteransApp.route('/')
 def index():
-    return veteransApp.send_static_file('index.html')
+    #return veteransApp.send_static_file('index.html')
     #return flask.send_file('index.html')
-    #return flask.make_response(open('veterans/templates/index.html').read())
+    return flask.make_response(open('veterans/static/index.html').read())
     #return flask.render_template("index.html")
 
 #from simple.views import *
